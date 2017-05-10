00f80fdf532386f4285ba9352b9c98a48e4c994b
helloworld3q3q <helloworld3q3q@gmail.com>
2017-05-10 17:54:49 +0800

    init

---
+++ index.android.js
@@ -0,0 +1,13 @@
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+
+export default class xinbomei extends Component {
+    rennder() {
+      return
+    }
+}
+
+
+AppRegistry.registerComponent('xinbomei', () => xinbomei);

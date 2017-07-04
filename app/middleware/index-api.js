49b4dad5e5bbd58baf767a2304445c63fa182faa
helloworld3q3q <helloworld3q3q@gmail.com>
2017-07-04 11:37:35 +0800

    index-api 多余删除

--- app/middleware/index-api.js
+++ app/middleware/index-api.js
@@ -6,20 +6,11 @@
  * @desc [description]
 */
 
-
-// const hotshow = 'http://192.168.0.103:8080/weixin/hotshow.json';
-// const sonshow = 'http://192.168.0.103:8080/weixin/sonshow.json';
-// const usshow = 'http://192.168.0.103:8080/weixin/usshow.json';
-// const nearcinemas = 'http://192.168.0.103:8080/weixin/nearcinemas.json';
-
-import { initHotshow, fetchLoading } from '../actions/hotshow-action';
-
 export function initFetch(action) {
 	return (url) => {
 		return (dispatch) => {fetch(url).then(res => res.json())
 			.then(json => {
 				dispatch(action(json));
-				//dispatch(fetchLoading(false));
 			}).catch(msg => console.log('usshowList-err  '+ msg));
 		}
 	}
